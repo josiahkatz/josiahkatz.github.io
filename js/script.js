7e1b0b9876af4192b241f51900345ee7a8174f7b
Josiah <josiahkatz@gmail.com>
2015-10-17 13:01:23 -0400

    load more shots and higher-res when possible

--- js/script.js
+++ js/script.js
@@ -5,13 +5,19 @@
 $.jribbble.setToken('125d175028de31e8fd3ec4d505b3ff4ba28d18256ce67327a9d7d73130e51bb4');
 
 
-$.jribbble.users('josiahkatz').shots({per_page: 12}).then(function(shots) {
+$.jribbble.users('josiahkatz').shots({per_page: 20}).then(function(shots) {
   var html = [];
   
   var list = $('.shots-list');
 
   shots.forEach(function(shot) {
-    html.push('<li class="shots-item"><a href="' + shot.html_url + '" class="shot" target="blank"><img src="' + shot.images.normal + '"/></a></li>');
+
+    if (shot.images.hidpi) {
+        var imgSrc = shot.images.hidpi;
+    } else {
+        var imgSrc = shot.images.normal;
+    }
+    html.push('<li class="shots-item"><a href="' + shot.html_url + '" class="shot" target="blank"><img src="' + imgSrc + '"/></a></li>');
   });
   
   $('.shots-list').html(html.join(''));
