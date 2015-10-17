c0187fa3c4ec2a7e3d6a004058c89c78501eb11e
Josiah <josiahkatz@gmail.com>
2015-10-17 12:42:53 -0400

    getting dribbble api set back up, not loading as elegantly, but I'll fix that eventually

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
-// @codekit-prepend "jquery-2.1.1.js";
-// @codekit-prepend "highlightReel.js";
+// @codekit-prepend "jquery.js";
+// @codekit-prepend "jribbble.js";
 /* // @codekit-prepend "last.fm.js"; */
 
-$("#shots").highlightReel({
-    username : "josiahkatz",
-    count:12,
-    stats: false,
-    title: false,
-    teaser: true,
-    linked: true
+$.jribbble.setToken('125d175028de31e8fd3ec4d505b3ff4ba28d18256ce67327a9d7d73130e51bb4');
+
+
+$.jribbble.users('josiahkatz').shots({per_page: 12}).then(function(shots) {
+  var html = [];
+  
+  var list = $('.shots-list');
+
+  shots.forEach(function(shot) {
+    html.push('<li class="shots-item"><a href="' + shot.html_url + '" class="shot" target="blank"><img src="' + shot.images.normal + '"/></a></li>');
+  });
+  
+  $('.shots-list').html(html.join(''));
 });
+
 
 /*$('#albums').lfm({
    APIkey: 'c457b724999968d805b9c964aa45485e',
